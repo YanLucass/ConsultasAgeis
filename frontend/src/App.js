8ec62ebc25ce4ef16e50bb894b4b262163e4360a
YanLucass <yanlucascarvalho18@gmail.com>
2024-01-13 16:49:55 -0300

    Adicionando NavBar, link para cadastrar pacientes, marcar consultas.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,15 +12,19 @@ import Message from './components/layout/Message';
 //pages
 import Home from './components/pages/Home';
 import PatientsRegister from './components/pages/PatientsRegister';
+import NavBar from './components/layout/NavBar';
+import ShowPatients from './components/pages/ShowPatients';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
     <Message />
       <Container>
         <Routes>
             <Route path = "/" element={<Home/>} />
             <Route path = "/patients/register" element={<PatientsRegister />} />
+            <Route path = "/showPatients" element={<ShowPatients />} />
         </Routes>
       </Container>
     </BrowserRouter>
---
+++ frontend/src/components/pages/ShowPatients.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import api from "../../utils/api";
+import { Link } from "react-router-dom";
+import styles from './ShowPatients.module.css'
+
+function ShowPatients() {
+
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        async function fetchPatients() {
+            const response = await api.get('/patients/all');
+           setPatients(response.data.patients);
+        }
+
+        fetchPatients();
+    }, []);
+
+    return (
+
+        <div className={styles.central}>
+        <div>   
+            <h2>Pacientes cadastrados:</h2>
+            <p>Clique para marcar consulta</p>
+        </div>
+
+        {/* show all patients registed */}
+        {patients.map((patient, index) => (
+            <div className={styles.patient} key={index}> 
+                <Link to={`/make/appointment/${patient.id}`}>
+                    {patient.id}<span>{patient.name}</span>
+                </Link>
+            </div>
+        ))};
+        
+        </div>   
+    )
+}
+
+export default ShowPatients;
